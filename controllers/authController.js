a1d8431ecd9f546e98e74ad92b85e50756fb5883
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-07-27 22:01:28 +0300

    I added custom conditions and added endpoint to organizer register page

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,16 +6,50 @@ const Organization = require('../models/Organization');
 
 exports.createUser = async (req, res) => {
   try {
+    const { telephone, email } = req.body;
+
+
+    // Eğer telefon numarası daha önce kullanıldıysa ve e-posta kullanılmadıysa
+    if (telephone) {
+      const existingUserWithTelephone = await User.findOne({ telephone });
+      if (existingUserWithTelephone) {
+        return res.status(400).json({
+          status: 'fail',
+          message: 'Bu telefon numarası zaten kullanılıyor.',
+        });
+      }
+    }
+
+    // Eğer e-posta adresi daha önce kullanıldıysa ve telefon numarası kullanılmadıysa
+    if (email) {
+      const existingUserWithEmail = await User.findOne({ email });
+      if (existingUserWithEmail) {
+        return res.status(400).json({
+          status: 'fail',
+          message: 'Bu e-posta adresi zaten kullanılıyor.',
+        });
+      }
+    }
+    if (telephone && email) {
+      const existingUserWithTelephone = await User.findOne({ telephone });
+      const existingUserWithEmail = await User.findOne({ email });
+    if (existingUserWithTelephone && existingUserWithEmail) {
+     return res.status(400).json({
+      status:'fail',
+      message:"telefon ve mail kullanımda"
+     })
+    }
+  }
+
+    // Yeni kullanıcı oluşturuluyor
     const user = await User.create(req.body);
-    //try-catch yapmamızın nedeni hatayı yakalamak içi
+
     res.status(201).json({
       status: 'success',
       user,
     });
   } catch (error) {
     res.status(400).json({
-      //Hatalı request gönderilirse dönecek
-      //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
       status: 'fail',
       error,
     });
@@ -29,7 +63,7 @@ exports.loginUser = async (req, res) => {
     if (!user) {
       return res.status(400).json({
         status: 'fail',
-        error: 'No such user found',
+        error: 'Kayitli kullanici bulunamadi',
       });
     }
     const same = await bcrypt.compare(password, user.password); //kullanıcının girdiği şifrenin, veritabanındaki kullanıcının şifresiyle eşleşip eşleşmediğini kontrol eder,Karşılaştırma sonucu same değişkenine atanır.
@@ -43,7 +77,7 @@ exports.loginUser = async (req, res) => {
     } else {
       res.status(400).json({
         status: 'fail',
-        error: 'No such user found',
+        error: 'Girdiginiz şifre yanlis',
       });
     }
   } catch (error) {
--- routes/organizerRoute.js
+++ routes/organizerRoute.js
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.route('/:id').get(organizationController.getOrganizer);//http://localhost:5000/organizer/:id
 router.route('/login').post(organizationController.loginOrganizer);//http://localhost:5000/organizer/login
+router.route('/organizerRegister').post(organizationController.createOrganizer);//http://localhost:5000/organizer/organizationRegister
 
 module.exports = router;
