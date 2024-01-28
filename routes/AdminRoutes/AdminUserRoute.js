b81230a9c3ef9f8b97f7cd65bb8309f4e8541ade
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2024-01-28 21:14:04 +0300

    fix: I added fix

--- routes/AdminRoutes/AdminUserRoute.js
+++ routes/AdminRoutes/AdminUserRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.route('/:id').get(authController.getUser); //http://localhost:5000/AdminUser/:id
 router.route('/login').post(AdminUserController.loginAdmin); //http://localhost:5000/AdminUser/login
-router.route('/Admin/AllUsers').get(authController.getAllUsers); //http://localhost:5000/AdminUser/Admin/AllUsers
+router.route('/Admin/AllUsers').get(authController.getAllUsers); //http://localhost:5000/AdminUser/Admin/AllUsers?page=1
 router.route('/:id/deleteUser').delete(authMiddleware.checkAdminAuthorization,AdminUserController.deleteUser); //http://localhost:5000/AdminUser/:id/deleteUser (userId)
 router.route('/:id/bannedUser').patch(authMiddleware.checkAdminAuthorization,AdminUserController.bannedUser); //http://localhost:5000/AdminUser/:id/bannedUser (userId)
 
